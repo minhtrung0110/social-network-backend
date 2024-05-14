dc805a7211dd907412927284a58568dc4638d6fc
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2024-05-14 10:34:17 +0700

    feat(src): add http utils and types
    update utils function
    add common type, schema and constrants
    add http util

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -14,6 +14,19 @@ export function convertNumbers(obj: ParamObj): ParamObj {
   return newObj;
 }
 
+export function convertRealType(obj: ParamObj): ParamObj {
+  const newObj: ParamObj = { ...obj };
+  for (const key in newObj) {
+    if (typeof newObj[key] === 'string' && !isNaN(parseInt(newObj[key] as string))) {
+      newObj[key] = parseInt(newObj[key] as string);
+    }
+    if (newObj[key] === 'null') {
+      newObj[key] = null;
+    }
+  }
+  return newObj;
+}
+
 export const getExpiry = () => {
   const createdAt = new Date();
   const expiresAt = moment(createdAt).add(5, 'minutes').toDate();
